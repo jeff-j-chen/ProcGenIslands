def82df1bf0a0af2eab25d45e0689b6ed69e94cd
4a454646 <4a.45.46.46@gmail.com>
2020-03-17 18:39:14 -0700

    Revert "Revert "last working save, about to make a big change""
    This reverts commit 0791c96c4d1b67834e8ad0a92f6a1ab35bacec23.

---
+++ Editor/Autosave.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+ 
+[InitializeOnLoad]
+public class Autosave
+{
+    static Autosave()
+    {
+        EditorApplication.playmodeStateChanged += () =>
+        {
+            if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+            {
+                Debug.Log("Auto-saving all open scenes...");
+                EditorSceneManager.SaveOpenScenes();
+                AssetDatabase.SaveAssets();
+            }
+        };
+    }
+}
