3539bd10d87ad326ef98dcfd6fb3908c06b2dae1
4a454646 <4a.45.46.46@gmail.com>
2020-03-17 20:12:48 -0700

    change height and width both to size

--- Scripts/MapGenerator.cs
+++ Scripts/MapGenerator.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class MapGenerator : MonoBehaviour {
     public enum DrawMode { NoiseMap, ColorMap, FalloffMap };
     public DrawMode drawMode;
-	public int mapWidth;
-	public int mapHeight;
+	public int mapSize;
 	public float noiseScale;
 	public int octaves;
 	[Range(0,1)]
@@ -19,12 +18,12 @@ public class MapGenerator : MonoBehaviour {
     private float[,] falloffMap;
 
 	public void GenerateMap() {
-		float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+		float[,] noiseMap = Noise.GenerateNoiseMap(mapSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
         // generate the noise map with the given variables
-        Color[] colorMap = new Color[mapWidth * mapHeight];
+        Color[] colorMap = new Color[mapSize * mapSize];
         // make a new colormap to apply colors to
-        for (int y = 0; y < mapHeight; y++) {
-            for (int x = 0; x < mapWidth; x++) {
+        for (int y = 0; y < mapSize; y++) {
+            for (int x = 0; x < mapSize; x++) {
                 // for every coordinate
                 if (applyFalloffMap) {
                     noiseMap[x,y] -= falloffMap[x,y];
@@ -36,7 +35,7 @@ public class MapGenerator : MonoBehaviour {
                     // for every region
                     if (currentHeight <= regions[i].height) {
                         // found the region that the point belongs to 
-                        colorMap[y * mapWidth + x] = regions[i].color;
+                        colorMap[y * mapSize + x] = regions[i].color;
                         // assign the color at given point to the colormap
                         break;
                         // no need to check other regions, so break out
@@ -48,19 +47,18 @@ public class MapGenerator : MonoBehaviour {
 		MapDisplay display = FindObjectOfType<MapDisplay>();
         // get the display script
         if (drawMode == DrawMode.NoiseMap) { display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap)); }
-        else if (drawMode == DrawMode.ColorMap) { display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight)); }
-        else if (drawMode == DrawMode.FalloffMap) { TextureGenerator.TextureFromHeightMap(FalloffGenerator.GenerateFalloffMap(mapWidth)); }
+        else if (drawMode == DrawMode.ColorMap) { display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapSize)); }
+        else if (drawMode == DrawMode.FalloffMap) { TextureGenerator.TextureFromHeightMap(FalloffGenerator.GenerateFalloffMap(mapSize)); }
         // draw the noise or color map on the plane, based on which one we want
 	}
 
 	void OnValidate() {
         // called every time an inspector variable is updated
-		if (mapWidth < 1) { mapWidth = 1; }
-		if (mapHeight < 1) { mapHeight = 1; }
+		if (mapSize < 1) { mapSize = 1; }
 		if (lacunarity < 1) { lacunarity = 1; }
 		if (octaves < 0) { octaves = 0; }
         // limit some variables
-        falloffMap = FalloffGenerator.GenerateFalloffMap(mapWidth);
+        falloffMap = FalloffGenerator.GenerateFalloffMap(mapSize);
 	}
 }
 
