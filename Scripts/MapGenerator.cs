b4f91398c84dc38e678fae33ade52461ff42012d
4a454646 <4a.45.46.46@gmail.com>
2020-03-19 11:44:42 -0700

    add more detailted comments

--- Scripts/MapGenerator.cs
+++ Scripts/MapGenerator.cs
@@ -4,24 +4,37 @@ using System.Collections.Generic;
 
 public class MapGenerator : MonoBehaviour {
 	public int mapSize;
+    // size of the map to create (size x size) 
 	public float noiseScale;
+    // scale of the noise to create, bigger number is bigger noise
 	public int octaves = 7;
+    // # of iterations of perlin noises to stack
 	[Range(0,1)]
-	public float persistance;
+	public float persistence;
+    // how much each level of perlin noise affects the next one, higher is a greater effect
 	public float lacunarity;
+    // the factor to increase the frequency by every octave
     public float hueFrequency;
+    // frequency of the noise to create, bigger number is bigger noise
 	public int seed;
+    // the seed to pass in
 	public Vector2 offset;
+    // the offset of the created map
 	public bool autoUpdate;
+    // whether or not to automatically update the map every time something changes
     public bool applyHueRegions;
+    // whether or not to apply the hue (green/blue) regions onto the map
     public float hueStrength;
+    // the strength of the hue to apply, bigger number is smaller effect
     public TerrainType[] regions;
+    // array of structure which manages the colors applied to varying heights
     private float[,] hueMap;
+    // float array representing the hue map
 
 	public void GenerateMap() {
         if (applyHueRegions) { hueMap = Noise.GenerateHueMap(mapSize, seed, noiseScale, hueFrequency, offset); }
         // print("finished hue");
-		float[,] noiseMap = Noise.GenerateNoiseMap(mapSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
+		float[,] noiseMap = Noise.GenerateNoiseMap(mapSize, seed, noiseScale, octaves, persistence, lacunarity, offset);
         // print("finished noise map");
         // generate the noise map with the given variables
         Color[] colorMap = new Color[mapSize * mapSize];
@@ -40,7 +53,9 @@ public class MapGenerator : MonoBehaviour {
                         Color newColor = regions[i].color;
                         if (applyHueRegions && currentHeight <= 0.2 && hueMap[x, y] > 0) {
                             Color.RGBToHSV(newColor, out H, out S, out V);
+                            // get the HSV variables from the color
                             newColor = Color.HSVToRGB(H - hueMap[x, y]/hueStrength, S, V);
+                            // use the hsv variables to create a new color, but with modified hue (make it more green or blue)
                         }
                         colorMap[y * mapSize + x] = newColor;
                         // assign the color at given point to the colormap
--- Scripts/Noise.cs
+++ Scripts/Noise.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 public static class Noise {
-	public static float[,] GenerateNoiseMap(int mapSize, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset) {
+	public static float[,] GenerateNoiseMap(int mapSize, int seed, float scale, int octaves, float persistence, float lacunarity, Vector2 offset) {
         float[,] noiseMap = new float[mapSize, mapSize];
         // start off with a noise map with the set dimensions
         if (scale <= 0) {
@@ -9,33 +9,35 @@ public static class Noise {
             // clamp the scale, so no division by 0 or negative problems
         }
 		System.Random prng = new System.Random(seed);
-        // pseudo random number generator
+        // pseudo random number generator with given seed
 		Vector2[] octaveOffsets = new Vector2[octaves];
         // want each octave to be taken from a different location
 		for (int i = 0; i < octaves; i++) {
             // for each octave count
 			float offsetX = prng.Next(-10000, 10000) + offset.x;
-			float offsetY = prng.Next(-10000, 10000) - offset.y;
+			float offsetY = prng.Next(-10000, 10000) + offset.y;
             // get a new location
 			octaveOffsets[i] = new Vector2(offsetX, offsetY);
             // add a place for the octave to be created
 		}
+        float halfSize = mapSize / 2f;
+        //
         for (int y = 0; y < mapSize; y++) {
             for (int x = 0; x < mapSize; x++) {
-                // for every coordate (x, y)
+                // for every coordinate (x, y)
                 float amplitude = 1;
                 float frequency = 1;
                 float noiseHeight = 0;
-                // variables to be used
+                // variables to be used, altered over every octave
 				for (int i = 0; i < octaves; i++) {
                     // for every octave
-					float sampleX = x / scale * frequency + octaveOffsets[i].x;
-					float sampleY = y / scale * frequency + octaveOffsets[i].y;
+					float sampleX = (((x-halfSize) / scale) * frequency) + octaveOffsets[i].x;
+					float sampleY = (((y-halfSize) / scale) * frequency) + octaveOffsets[i].y;
                     // alter coords based on the octave offset
                     float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
                     // use builtin perlin noise generation, but change it from 0-1 to -1 to 1
                     noiseHeight += perlinValue * amplitude;
-                    amplitude *= persistance;
+                    amplitude *= persistence;
                     frequency *= lacunarity;
                     // modify the variables, watch sebastian lague's 1st terrain generation if you forget
 				}
@@ -51,12 +53,14 @@ public static class Noise {
 
 	public static float[,] GenerateHueMap(int mapSize, int seed, float scale, float frequency, Vector2 offset) {
         float[,] noiseMap = new float[mapSize, mapSize];
+        // create a new map
         if (scale <= 0) {
             scale = 0.0001f;
         }
+        // limit the scale
 		System.Random prng = new System.Random(seed * 2);
-        float offsetX = prng.Next(-100000, 100000) + offset.x;
-        float offsetY = prng.Next(-100000, 100000) + offset.y;
+        float offsetX = prng.Next(-10000, 10000) + offset.x;
+        float offsetY = prng.Next(-10000, 10000) + offset.y;
         for (int y = 0; y < mapSize; y++) {
             for (int x = 0; x < mapSize; x++) {
                 float sampleX = x / scale * frequency + offsetX;
