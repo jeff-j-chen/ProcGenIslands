8c1d97c9b07b77df5069a30b71dfcb795cdab6a0
4a454646 <4a.45.46.46@gmail.com>
2020-04-02 21:22:26 -0700

    first import of menu screen

---
+++ Scripts/PlayerData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public float[] position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public PlayerData(Player player)
+    {
+        position = new float[3];
+        position[0] = player.transform.position.x;
+        position[1] = player.transform.position.y;
+        position[2] = player.transform.position.z;
+    }
+}
